b74a9016a344fdd9c568fe201752a0bb623e7fca
dueswert <59000200+dueswert@users.noreply.github.com>
2020-02-19 11:21:53 +0800

    New

---
+++ www/js/index.js
@@ -0,0 +1,9 @@
+var app = new Framework7({
+    // App root element
+    root: '#app',
+    // App Name
+    name: 'Vote App!',
+    // App id
+    id: 'voteapp.starfire.com',
+
+});
---
+++ www/js/script.js
@@ -0,0 +1,23 @@
+const selectElement =(element) => document.querySelector(element);
+
+const header = selectElement('header');
+const mainContent = selectElement('main');
+
+
+selectElement('.hamburger').addEventListener('click',() =>{
+    header.classList.toggle('active');
+    mainContent.classList.toggle('active');
+});
+
+
+//clicking outside to exit
+
+window.onclick = (event)=> {
+    if(event.target.matches('.active')){
+     
+        if(header.classList.contains('active')){
+            header.classList.remove('active');
+            mainContent.classList.remove('active');
+        }
+    }
+};
